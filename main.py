430170ef89b645ceecf90c71ee1ea66201a8a6f0
Joentze <Joentze01@gmail.com>
2022-03-09 22:27:10 +0800

    added flask api

---
+++ main.py
@@ -0,0 +1,29 @@
+from distutils.log import debug
+from flask import Flask, request
+from flask_restful import Resource, Api
+from scrape import get_hrefs, get_texts
+
+app = Flask(__name__)
+api = Api(app)
+
+class scrape_hrefs(Resource):
+    def get(self):
+        post_received = request.get_json()
+        link = post_received["link"]
+        print(link)
+        xpaths = post_received["xpaths"]
+        print(xpaths)
+        return {'hrefs':get_hrefs(link, xpaths)}
+
+class scrape_content(Resource):
+    def get(self):
+        post_received = request.get_json()
+        link = post_received["link"]
+        contents = post_received["article"]
+        return {'article':get_texts(link, contents)}
+
+api.add_resource(scrape_hrefs, '/scrape/href')
+api.add_resource(scrape_content, '/scrape/article')
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- scrape.py
+++ scrape.py
@@ -8,35 +8,45 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 #chrome_options.add_argument("--disable-extensions")
 #chrome_options.add_argument("--disable-gpu")
-#chrome_options.add_argument("--no-sandbox") # linux only
 chrome_options.add_argument("--headless")
 
 PATH = "./driver/chromedriver"
 
 #driver = webdriver.Chrome(PATH, options=chrome_options)
+
 driver = webdriver.Chrome(PATH)
+def get_hrefs(link:str, xpaths:list)->list:
+    links = []
+    driver.get(link)
+    try:
+        for xpath in xpaths:
+            try:
+                element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, xpath))
+                )
+                links.append(element.get_attribute('href'))
+            except:
+               pass
+    finally:
+        driver.get("data:,")
+    return links
 
-
-driver.get("https://news.kompas.com/")
-
-payload = [
-    "/html/body/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/ul/div/div/li[1]/a",
-]
-
-links = []
-
-try:
-    for load in payload:
-        try:
-            element = WebDriverWait(driver, 30).until(
-                EC.presence_of_element_located((By.XPATH, load))
-            )
-            links.append(element.get_attribute('href'))
-        except:
-           pass
-finally:
-    print(links)
-    driver.quit()
+def get_texts(link:str, contents:object)->object:
+    response_content = {}
+    driver.get(link)
+    try:
+        for name, content in contents.items():
+            print(name, content)
+            try:
+                element = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.XPATH, content))
+                )
+                response_content[name] = element.get_attribute("innerHTML")
+            except:
+               pass
+    finally:
+        driver.get("data:,")
+    return response_content
 
 
 
