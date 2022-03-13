a3ea9b1d6c969f0aa72cbfe37bd55b61f280896c
Joen <63651091+Joentze@users.noreply.github.com>
2022-03-13 23:19:38 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -15,6 +15,7 @@ chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 PATH = "./driver/chromedriver"
 #driver = webdriver.Chrome(PATH)
