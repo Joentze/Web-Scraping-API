a1df3db71d79d3d49a8d01b87b00c5ee4650c383
Joentze <Joentze01@gmail.com>
2022-04-09 00:10:32 -0700

    commit test

--- main.py
+++ main.py
@@ -2,33 +2,44 @@ from distutils.log import debug
 from flask import Flask, request
 from flask_restful import Resource, Api
 from scrape import get_hrefs, get_texts, get_from_child_one, get_from_child_many
+from rq import Queue
+from worker import conn
+
+q = Queue(connection=conn)
 
 app = Flask(__name__)
 api = Api(app)
+
 
 class scrape_hrefs(Resource):
     def get(self):
         get_received = request.get_json()
         link = get_received["link"]
         xpaths = get_received["xpaths"]
-        return {'hrefs':get_hrefs(link, xpaths)}
+        return {'hrefs': get_hrefs(link, xpaths)}
+
 
 class scrape_content(Resource):
     def get(self):
         get_received = request.get_json()
         link = get_received["link"]
         contents = get_received["article"]
-        return {'article':get_texts(link, contents)}
+        return {'article': get_texts(link, contents)}
+
 
 class scrape_from_child_one(Resource):
     def get(self):
         get_received = request.get_json()
-        return {"results":get_from_child_one(get_received)}
+        results = q.enqueue(get_from_child_one, get_received)
+        return {"results": results}
+
 
 class scrape_from_child_many(Resource):
     def get(self):
         get_received = request.get_json()
-        return {"results":get_from_child_many(get_received)}
+        results = q.enqueue(get_from_child_many, get_received)
+        return {"results": results}
+
 
 class hello_world(Resource):
     def get(self):
@@ -37,8 +48,8 @@ class hello_world(Resource):
 
 api.add_resource(scrape_hrefs, '/scrape/href')
 api.add_resource(scrape_content, '/scrape/article')
-api.add_resource(scrape_from_child_one,'/scrape/from-child/one')
-api.add_resource(scrape_from_child_many,'/scrape/from-child/many')
+api.add_resource(scrape_from_child_one, '/scrape/from-child/one')
+api.add_resource(scrape_from_child_many, '/scrape/from-child/many')
 api.add_resource(hello_world, "/hello")
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape.py
+++ scrape.py
@@ -8,8 +8,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 import os
-GOOGLE_CHROME_PATH = os.environ["GOOGLE_CHROME_PATH"]
-CHROMEDRIVER_PATH = os.environ["CHROMEDRIVER_PATH"]
+#GOOGLE_CHROME_PATH = os.environ["GOOGLE_CHROME_PATH"]
+CHROMEDRIVER_PATH = "./drvier/chromedriver"  # os.environ["CHROMEDRIVER_PATH"]
 
 chrome_options = Options()
 chrome_options.add_argument("--disable-extensions")
@@ -18,12 +18,15 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument("--enable-javascript")
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+#chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 #PATH = "./driver/chromedriver"
 #driver = webdriver.Chrome(PATH)
-def get_hrefs(link:str, xpaths:list)->list:
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+
+
+def get_hrefs(link: str, xpaths: list) -> list:
+    driver = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH, options=chrome_options)
     driver.get(link)
     links = []
     try:
@@ -34,13 +37,15 @@ def get_hrefs(link:str, xpaths:list)->list:
                 )
                 links.append(element.get_attribute('href'))
             except:
-               pass
+                pass
     finally:
         driver.quit()
     return links
 
-def get_texts(link:str, contents:object)->object:
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+
+def get_texts(link: str, contents: object) -> object:
+    driver = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH, options=chrome_options)
     driver.get(link)
     response_content = {}
     try:
@@ -50,7 +55,8 @@ def get_texts(link:str, contents:object)->object:
                 element = WebDriverWait(driver, 5).until(
                     EC.presence_of_element_located((By.XPATH, content))
                 )
-                response_content[name] = filter_text(element.get_attribute("innerHTML"))
+                response_content[name] = filter_text(
+                    element.get_attribute("innerHTML"))
             except:
                 response_content["body"] = get_body_fallback(driver)
                 break
@@ -58,27 +64,31 @@ def get_texts(link:str, contents:object)->object:
         driver.quit()
     return response_content
 
-def get_from_child_one(grab_object:object)->list:
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+
+def get_from_child_one(grab_object: object) -> list:
+    driver = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH, options=chrome_options)
     driver.get(grab_object["link"])
     main_tag = grab_object["main_tag"]
     var = grab_object["identifier_type"]
     var_content = grab_object["identifier"]
-    wait_time=int(grab_object["wait_time"])
+    wait_time = int(grab_object["wait_time"])
     get_child = f"//{main_tag}[@{var}='{var_content}']//child::*"
     try:
         elements = WebDriverWait(driver, wait_time).until(
-                EC.presence_of_all_elements_located((By.XPATH, get_child))
-            )
-        links = [element.get_attribute(grab_object["attribute"]) 
-                for element in elements 
-                if element.tag_name == grab_object["tag"]]
+            EC.presence_of_all_elements_located((By.XPATH, get_child))
+        )
+        links = [element.get_attribute(grab_object["attribute"])
+                 for element in elements
+                 if element.tag_name == grab_object["tag"]]
     finally:
         driver.quit()
     return links
 
-def get_from_child_many(request_object:list)->list:
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
+
+def get_from_child_many(request_object: list) -> list:
+    driver = webdriver.Chrome(
+        executable_path=CHROMEDRIVER_PATH, options=chrome_options)
     grab_objects = request_object["grab_objects"]
     link = request_object["link"]
     driver.get(link)
@@ -89,19 +99,20 @@ def get_from_child_many(request_object:list)->list:
                 main_tag = grab_object["main_tag"]
                 var = grab_object["identifier_type"]
                 var_content = grab_object["identifier"]
-                wait_time=int(grab_object["wait_time"])
+                wait_time = int(grab_object["wait_time"])
                 get_child = f"//{main_tag}[@{var}='{var_content}']//child::*"
                 elements = WebDriverWait(driver, wait_time).until(
-                        EC.presence_of_all_elements_located((By.XPATH, get_child))
-                    )
-                results.append([element.get_attribute(grab_object["attribute"]) 
-                                for element in elements 
+                    EC.presence_of_all_elements_located((By.XPATH, get_child))
+                )
+                results.append([element.get_attribute(grab_object["attribute"])
+                                for element in elements
                                 if element.tag_name == grab_object["tag"]])
             except:
                 pass
     finally:
         driver.quit()
     return results
+
 
 def get_body_fallback(driver):
     print("trying something else")
@@ -113,11 +124,13 @@ def get_body_fallback(driver):
     except:
         pass
 
+
 def filter_text(text):
     soup = BeautifulSoup(text, "html.parser")
     texts = soup.find_all(text=True)
     filtered_text = list(filter(tag_visible, texts))
     return " ".join(filtered_text)
+
 
 def tag_visible(element):
     if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
@@ -125,7 +138,6 @@ def tag_visible(element):
     if isinstance(element, Comment):
         return False
     return True
-
 
 
 if __name__ == "__main__":
